b7fde2ad2e4056c70d485005e3cb3d86b599d074
Kirill <afanasiev.kirill@gmail.com>
2018-09-15 16:21:47 +0300

    Задание 4 Параметризация
    Задание 4 добавлена параметризация

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTest.java
@@ -1,61 +1,78 @@
 package ru.stqa.pft.addressbook;
 
-import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 import org.testng.annotations.*;
 import static org.testng.Assert.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
 
 public class ContactCreationTest {
   private WebDriver driver;
-  private String baseUrl;
   private boolean acceptNextAlert = true;
   private StringBuffer verificationErrors = new StringBuffer();
 
-  @BeforeClass(alwaysRun = true)
+  @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
     driver = new FirefoxDriver();
-    baseUrl = "https://www.katalon.com/";
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    driver.get("http://localhost/addressbook/");
+    Login("admin", "secret");
+  }
+
+  private void Login(String username, String password) {
+    driver.findElement(By.name("user")).clear();
+    driver.findElement(By.name("user")).sendKeys(username);
+    driver.findElement(By.name("pass")).clear();
+    driver.findElement(By.name("pass")).sendKeys(password);
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
   }
 
   @Test
-  public void testUntitledTestCase() throws Exception {
-    driver.get("http://localhost/addressbook/");
-    driver.findElement(By.name("user")).clear();
-    driver.findElement(By.name("user")).sendKeys("admin");
-    driver.findElement(By.name("pass")).clear();
-    driver.findElement(By.name("pass")).sendKeys("secret");
-    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
-    driver.findElement(By.linkText("add new")).click();
+  public void testContactCreation() throws Exception {
+    initContactCreation();
+    fillContactForm("Ivanov", "Ivan", "Ivanovich", "Ivan_01", "Moscow Red squre 1 h1", "+79153925555");
+    submitContactCreation();
+    returnToContactCreation();
+
+  }
+
+  private void returnToContactCreation() {
+    driver.findElement(By.linkText("home page")).click();
+  }
+
+  private void submitContactCreation() {
+    driver.findElement(By.name("submit")).click();
+  }
+
+  private void fillContactForm(String firstname, String middlename, String lastname, String nickname, String address, String homephone) {
     driver.findElement(By.name("firstname")).click();
     driver.findElement(By.name("firstname")).clear();
-    driver.findElement(By.name("firstname")).sendKeys("Ivanov");
+    driver.findElement(By.name("firstname")).sendKeys(firstname);
     driver.findElement(By.name("middlename")).click();
     driver.findElement(By.name("middlename")).clear();
-    driver.findElement(By.name("middlename")).sendKeys("Ivan");
+    driver.findElement(By.name("middlename")).sendKeys(middlename);
     driver.findElement(By.name("lastname")).click();
     driver.findElement(By.name("lastname")).clear();
-    driver.findElement(By.name("lastname")).sendKeys("Ivanovich");
+    driver.findElement(By.name("lastname")).sendKeys(lastname);
     driver.findElement(By.name("nickname")).click();
     driver.findElement(By.name("nickname")).clear();
-    driver.findElement(By.name("nickname")).sendKeys("Ivan_01");
+    driver.findElement(By.name("nickname")).sendKeys(nickname);
     driver.findElement(By.name("company")).click();
     driver.findElement(By.name("address")).click();
     driver.findElement(By.name("address")).clear();
-    driver.findElement(By.name("address")).sendKeys("Moscow Red squre 1 h1");
+    driver.findElement(By.name("address")).sendKeys(address);
     driver.findElement(By.name("home")).click();
     driver.findElement(By.name("home")).clear();
-    driver.findElement(By.name("home")).sendKeys("+79153925555");
-    driver.findElement(By.name("submit")).click();
-    driver.findElement(By.linkText("home page")).click();
-    driver.findElement(By.linkText("Logout")).click();
+    driver.findElement(By.name("home")).sendKeys(homephone);
   }
 
-  @AfterClass(alwaysRun = true)
+  private void initContactCreation() {
+    driver.findElement(By.linkText("add new")).click();
+  }
+
+  @AfterMethod(alwaysRun = true)
   public void tearDown() throws Exception {
+    driver.findElement(By.linkText("Logout")).click();
     driver.quit();
     String verificationErrorString = verificationErrors.toString();
     if (!"".equals(verificationErrorString)) {
