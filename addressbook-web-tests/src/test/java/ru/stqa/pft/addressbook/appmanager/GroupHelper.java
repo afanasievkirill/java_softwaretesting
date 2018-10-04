bace9f3a0d520b4c4fe3396c11a237af64e16855
Kirill <afanasiev.kirill@gmail.com>
2018-10-04 18:20:58 +0300

    Lection 5.5 deletion

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -45,6 +45,10 @@ public class GroupHelper extends HelperBase {
     wd.findElements(By.name("selected[]")).get(index).click();
   }
 
+  public void selectGroupById(int id) {
+    wd.findElement(By.cssSelector("input[value='"+id+"']")).click();
+  }
+
   public void initGroupModification() {
     click(By.name("edit"));
   }
@@ -70,6 +74,12 @@ public class GroupHelper extends HelperBase {
 
   public void delete(int index) {
     selectGroup(index);
+    deleteSelectedGroups();
+    returnToGroupPage();
+  }
+
+  public void delete(GroupData group) {
+    selectGroupById(group.getId());
     deleteSelectedGroups();
     returnToGroupPage();
   }
@@ -103,4 +113,5 @@ public class GroupHelper extends HelperBase {
     }
     return groups;
   }
+
 }
