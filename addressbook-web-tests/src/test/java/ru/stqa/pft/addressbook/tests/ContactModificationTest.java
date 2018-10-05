8f8c2f6fcc5191bbc2fca30803e4c02ec6ec22b6
Kirill <afanasiev.kirill@gmail.com>
2018-10-05 15:50:34 +0300

    Лекция 5-6

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTest.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public class ContactModificationTest extends TestBase {
 
-  @Test (enabled = false)
+  @Test 
   public void testContactModification() {
     app.goTo().gotoContactPage();
     if (!app.getContactHelper().isThereAContact()) {
